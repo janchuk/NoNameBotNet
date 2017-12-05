2850063dde7c9b321c94cf8bb9d7cf55179a22f4
RaphaelDG <Raphael.dulong@gmail.com>
2017-12-05 21:24:29 +0100

    Add HelloWorld

---
+++ BotNet/BotNet/BotNet.cpp
@@ -0,0 +1,16 @@
+// BotNet.cpp : définit le point d'entrée pour l'application console.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello je suis un botnet" << endl;
+	system("PAUSE");
+
+    return 0;
+}
+
