9e7b481eb64180b5f1bdd1f049759beed8ddc431
RaphaelDG <Raphael.dulong@gmail.com>
2018-04-05 12:30:47 +0200

    Executor

---
+++ BotNet/Botnet/Executor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Botnet
+{
+    class Executor
+    {
+        void protocolDecoder(string received)
+        {
+
+            int nbArg = Regex.Matches(received, "<ARG>").Count;
+            int nbArg1 = Regex.Matches(received, "<CMD>").Count;
+            
+        }
+    }
+}
