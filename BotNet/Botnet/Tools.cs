b8914da1bf0e3e869d872be12ba63044ad836aa7
Erawlam19 <richiardi.paul@gmail.com>
2018-04-05 12:12:50 +0200

    MAJ2

--- BotNet/Botnet/Tools.cs
+++ BotNet/Botnet/Tools.cs
@@ -27,5 +27,59 @@ namespace Botnet
                 return null;
             }
         }
+		
+		public string GetIPAddress()
+        {
+            string ip = null;
+
+            string PCUsername = Environment.UserName;
+            string chemin = "C:/users/" + PCUsername + "/Downloads/system_ip.txt";
+
+            IPHostEntry Host = default(IPHostEntry);
+            string Hostname = null;
+            Hostname = System.Environment.MachineName;
+            Host = Dns.GetHostEntry(Hostname);
+            foreach (IPAddress IP in Host.AddressList)
+            {
+                if (IP.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+                    ip = Convert.ToString(IP);
+                    string lines = ip;
+                    System.IO.File.WriteAllText(@chemin, lines);
+                }
+            }
+            return ip;
+        }
+
+
+        public void CheckIP()
+        {
+            string PCUsername = Environment.UserName;
+            string chemin = "C:/users/" + PCUsername + "/Downloads/system_ip.txt";
+
+            string IPactu = File.ReadAllText(chemin);
+
+            // Boucle infinie pour savoir si l@ IP de l'état précédent est tjrs la m que l'état actuelle
+            bool letsgo = true;
+            string IPprec = null;
+
+            while (letsgo == true)
+                {
+                IPprec = IPactu;
+                IPactu = GetIPAddress();
+
+                if (IPactu != IPprec)
+                    {
+                    Console.WriteLine("L'ancienne adresse est: " + IPprec);
+                    IPprec = IPactu;
+                    // Envoie de l'ancienne adresse IP + la nouvelle IP + l'adresse MAC
+                    Console.WriteLine("La nouvelle adresse est: " + IPactu);
+                    Console.ReadKey();
+                    // Send la nouvelle IP au serveur avec le nom, pour que côté serveur le script fasse matcher les deux
+                }
+                }
+        }
+        // Côté serveur cela devra faire le lien entre les différentes IP et l@ MAC du PC pour ne pas avoir de doublons
+        
     }
 }
