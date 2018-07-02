849054128d850ec074a6158a56e516e79e1f19e7
yaje996 <yaje996@users.noreply.github.com>
2018-07-02 22:52:08 +0200

    new_process

---
+++ process/process/Program_process.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+
+namespace process
+{
+    class Program_process
+    {
+
+        static void Main(string[] args)
+        {
+            Process[] pname = Process.GetProcessesByName("process2");
+            Console.WriteLine(pname.Length);
+            
+            Process Myprocess = new Process();
+            Myprocess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            Myprocess.StartInfo.FileName = @"C:\Users\yann\source\repos\process\process2\bin\Debug\process2.exe";
+            pname = Process.GetProcessesByName("process2");
+           
+            try
+            {
+                while(true)
+                {
+                    pname = Process.GetProcessesByName("process2");
+                    if (pname.Length == 0)
+                    {
+                        
+                        Myprocess.Start();
+                       
+                       
+                        Console.WriteLine(pname.Length);
+                        
+                    }
+                    
+                }
+
+                
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
---
+++ process/process2/Program_process2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace process2
+{
+    class Program_process2
+    {
+        static void Main(string[] args)
+        {
+            Process[] pname = Process.GetProcessesByName("process");
+            Console.WriteLine(pname.Length);
+
+            Process Myprocess = new Process();
+            Myprocess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            Myprocess.StartInfo.FileName = @"C:\Users\yann\source\repos\process\process\bin\Debug\process.exe";
+            pname = Process.GetProcessesByName("process");
+
+            try
+            {
+                while (true)
+                {
+                    pname = Process.GetProcessesByName("process");
+                    if (pname.Length == 0)
+                    {
+
+                        Myprocess.Start();
+
+
+                        Console.WriteLine(pname.Length);
+
+                    }
+
+                }
+
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+            }
+        }
+    }
+}
